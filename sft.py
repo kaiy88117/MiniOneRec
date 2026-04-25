cbe412f42b4861b25db8eb39f05638a24ac5be9d
kaiy88117 <kaiy88117@gmail.com>
2026-04-25 19:40:24 +0800

    fix long-tail loss weight propagation

--- sft.py
+++ sft.py
@@ -356,6 +356,7 @@ def train(
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
             report_to=None,
+            remove_unused_columns=False,
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
